ed25c3833f963e653e77778a858f817ac88d55ae
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-01-19 08:45:10 -0300

    using xlsx utils to conver to json

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+const app = require("express")();
 const read = require("./src/readXLSX.js");
 
-async function run(){
+
+// app.get("/", async (req, res) => {
+//     const data = await read("./files/DalyRewards.xlsx", 4);
+//     res.json(data);
+// })
+
+// app.listen(3000, () => console.log("SERVER RUNNING AT http://localhost:3000"));
+
+async function run() {
     const data = await read("./files/DalyRewards.xlsx", 4);
     console.log(data);
-
 }
 run()
--- src/readXLSX.js
+++ src/readXLSX.js
@@ -4,68 +4,14 @@ const read = (xlsxPath, columns) => new Promise((resolve, reject) => {
     try {
         //catch file
         const workBook = XLSX.readFile(xlsxPath);
-
-
-        const titles = []
-        const datas = []
-
-        let loops = 0;
-        for (var attr in workBook.Sheets.Plan1) {
-            if (!workBook.Sheets.Plan1[attr].v) {
-                loops++;
-                continue;
-            }
-
-            if (loops < columns + 1) {
-                titles.push(workBook.Sheets.Plan1[attr].v);
-                loops++;
-                continue;
-            }
-
-            datas.push(workBook.Sheets.Plan1[attr].v);
-
-            loops++;
-        }
-
-        resolve(toJson(titles, datas, columns));
-        //resolve({titles, datas});
+        const worksheet = workBook.Sheets[workBook.SheetNames[0]];
+        
+        const data = XLSX.utils.sheet_to_json(worksheet)
+        resolve (data);
     }
     catch (e) {
         reject(e);
     }
-
 })
-
-function toJson(titles, datas) {
-    const data = {};
-    for (let i in datas) {
-        data[titles[i]] = datas[i];
-    }
-    return data;
-}
-
-function toJson(titles, datas, columns) {
-    const newData = [];
-    for (let i = 0; i < datas.length; i += columns) {
-        const newObject = {};
-        for (let j = 0; j < titles.length; j++) {
-            newObject[titles[j]] = datas[i + j];
-        }
-        newData.push(newObject);
-    }
-    return newData;
-}
-
-function divideInSubArrays(arr, dataPerArr) {
-    const subArr = [];
-    for (let i = 1; i < arr.length; i += dataPerArr) {
-        subArr.push(arr.slice(i, i + dataPerArr));
-    }
-
-    return subArr
-
-
-
-}
 
 module.exports = read;
