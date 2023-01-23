53a66e105a558da4eb5ccd26bbc9f00562d8962b
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-01-23 10:49:14 -0300

    reading files from web now

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const app = require("express")();
-const read = require("./src/readXLSX.js");
+const {read, readOnline} = require("./src/readXLSX.js");
+const fetch = require("node-fetch");
 
-app.get("/:fileName", async (req, res) => {
+app.get("/file/:fileName", async (req, res) => {
     const fileName = req.params.fileName;
     
     if(!fileName) return res.status(400).send("No filename provided")
 
     try{
-        const data = await read(`./files/${fileName}.xlsx`, 4);
+        const data = await read(`./files/${fileName}.xlsx`);
         return res.json(data);
+    }
+    catch(e){
+        return res.status(500).send(e.message);
+    }
+})
+
+app.get("/url", async (req, res) => {
+    const url = req.query.url;
+
+    if(!url) return res.status(400).send("no url provided")
+
+    const response = await fetch(url, {
+        method: "GET",
+        responseType: "arraybuffer"
+    });
+    
+    const data = await response.arrayBuffer();
+    
+    // if(!fileName) return res.status(400).send("No filename provided")
+    
+    try{
+        const objData = await readOnline(data);
+        return res.json(objData);
     }
     catch(e){
         return res.status(500).send(e.message);
--- src/readXLSX.js
+++ src/readXLSX.js
@@ -1,17 +1,78 @@
 XLSX = require('xlsx');
 
-const read = (xlsxPath) => new Promise((resolve, reject) => {
-    try {
-        //catch file
-        const workBook = XLSX.readFile(xlsxPath);
-        const worksheet = workBook.Sheets[workBook.SheetNames[0]];
-        
-        const data = XLSX.utils.sheet_to_json(worksheet)
-        resolve (data);
-    }
-    catch (e) {
-        reject(e);
-    }
-})
+async function read(xlsxPath) {
+    return await new Promise((resolve, reject) => {
+        try {
+            //catch file
+            const workBook = XLSX.readFile(xlsxPath);
+            const worksheet = workBook.Sheets[workBook.SheetNames[0]];
 
-module.exports = read;
+            const data = XLSX.utils.sheet_to_json(worksheet)
+            resolve(data);
+        }
+        catch (e) {
+            reject(e);
+        }
+    })
+}
+
+async function readOnline(data) {
+    return await new Promise((resolve, reject) => {
+        try {
+            //catch file
+            const workBook = XLSX.read(data);
+            const worksheet = workBook.Sheets[workBook.SheetNames[0]];
+            const res = XLSX.utils.sheet_to_json(worksheet)
+
+            res.forEach(row => {
+                delete row.__EMPTY;
+            });
+
+            let converted = convertObject(res)
+
+            resolve(converted);
+        }
+        catch (e) {
+            reject(e);
+        }
+    })
+}
+
+function convertObject(inputArray) {
+    // Get the keys from the first object
+    const keys = Object.keys(inputArray[0]);
+
+    // Initialize an empty array for the output
+    const outputArray = [];
+
+    // Iterate over the input array
+    for (let i = 1; i < inputArray.length; i++) {
+        // Initialize an empty object for the current output
+        const outputObject = {};
+
+        // Iterate over the keys
+        for (let j = 0; j < keys.length; j++) {
+            // Get the key and value for the current object
+            const key = keys[j];
+            const value = inputArray[i][key];
+
+            // If the value is not empty, add it to the output object
+            if (value) {
+                outputObject[inputArray[0][key]] = value;
+            }
+        }
+
+        // If the output object is not empty, add it to the output array
+        if (Object.keys(outputObject).length > 0) {
+            outputArray.push(outputObject);
+        }
+    }
+
+    return outputArray;
+}
+
+
+module.exports = {
+    read,
+    readOnline
+};
